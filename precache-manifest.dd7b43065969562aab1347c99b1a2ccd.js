fb814ca59468207bbe91401d3246dfa3d569abc9
minjoo cha <minjoo.cha@qileap.com>
2018-11-07 16:26:14 -0800

    Updates

---
+++ precache-manifest.dd7b43065969562aab1347c99b1a2ccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19c99e42f99322c8db66",
+    "url": "/maps/static/css/main.0ea2f448.chunk.css"
+  },
+  {
+    "revision": "5707f31027f147ca94fe",
+    "url": "/maps/maps/static/js/runtime~main.5707f310.js"
+  },
+  {
+    "revision": "19c99e42f99322c8db66",
+    "url": "/maps/maps/static/js/main.19c99e42.chunk.js"
+  },
+  {
+    "revision": "da1b34cbe0d9890cf257",
+    "url": "/maps/maps/static/js/1.da1b34cb.chunk.js"
+  },
+  {
+    "revision": "0c6efbbd98a0237c286c26cd451418f4",
+    "url": "/maps/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc556936af38090422109b31218e04c8.js"
+  "/maps/precache-manifest.dd7b43065969562aab1347c99b1a2ccd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/maps/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
